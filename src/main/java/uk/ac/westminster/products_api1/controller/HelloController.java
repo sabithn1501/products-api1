bfbceb5b10f5884ee905065d8950b9a425bdd18c
sabithn1501 <sabithnaizer@gmail.com>
2026-09-25 12:19:31 +0530

    commit 2

--- src/main/java/uk/ac/westminster/products_api1/controller/HelloController.java
+++ src/main/java/uk/ac/westminster/products_api1/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring !";
     }
     @GetMapping("/status")
     public String status() {
